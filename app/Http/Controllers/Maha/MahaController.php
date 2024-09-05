100875398b6b7f97709ccd267e7b84f73d60026d
batrisyia1008 <nurbatrsiyia6688@gmail.com>
2024-09-05 10:00:51 +0800

    first commit

---
+++ app/Http/Controllers/Maha/MahaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Maha;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MahaController extends Controller
+{
+    public function welcome(){
+        return response()->view('maha.welcome');
+    }
+
+    public function register(){
+        return response()->view('maha.register');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\Maha\MahaController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function(){
+    return 'Test';
+});
+Route::get('welcome', [MahaController::class, 'welcome'])->name('welcome');
+Route::get('register-form', [MahaController::class, 'register'])->name('register-form');
